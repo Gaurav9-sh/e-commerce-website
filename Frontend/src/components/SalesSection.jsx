5c5060b4235849cc47cb90a9c502f1469b00be7a
Gaurav Sharma <gauravsh650@gmail.com>
2024-03-14 22:47:09 +0530

    enabling scroll bar

--- Frontend/src/components/SalesSection.jsx
+++ Frontend/src/components/SalesSection.jsx
@@ -1,12 +1,100 @@
+// import React, { useEffect, useRef, useState } from "react";
+// import "./saleSection.css";
+// import Card from "./Card";
+// import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+// import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+// import axios from "axios";
+
+
+// const SalesSection = () => {
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get("https://e-commerce-backend-opis.onrender.com/api/products");
+//         setProducts(response.data);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   const boxRef = useRef(null);
+
+//   const btnpressnext = () => {
+//     console.log("Button is pressed")
+//     if (boxRef.current) {
+//       console.log("It should move")
+//       let width = boxRef.current.clientWidth / 5;
+//       boxRef.current.scrollLeft = boxRef.current.scrollLeft + width;
+//       boxRef.current.scrollBy({ left: width, behavior: "smooth" });
+//     }
+//   };
+
+//   const btnpressprev = () => {
+//     if (boxRef.current) {
+//       let width = boxRef.current.clientWidth / 5;
+//       boxRef.current.scrollLeft = boxRef.current.scrollLeft - width;
+//       boxRef.current.scrollBy({ left: -width, behavior: "smooth" });
+//     }
+//   };
+
+//   const getRandomProducts = () => {
+//     const startIndex = Math.floor(Math.random() * (products.length - 7));
+//     return products.slice(startIndex, startIndex + 7);
+//   };
+
+//   return (
+//     <div className="saleSection">
+//       <div className="writingSection">
+//         <h4 className="heading4">Today's</h4>
+//         <div className="slidingClass">
+//           <h1 className="heading1">Flash Sales</h1>
+//           <div className="buttonClass">
+//             <button
+//               style={{ width: "40px", height: "40px" }}
+//               onClick={btnpressprev}
+//             >
+//               <NavigateBeforeIcon />
+//             </button>
+//             <button
+//               style={{ width: "40px", height: "40px" }}
+//               onClick={btnpressnext}
+//             >
+//               <NavigateNextIcon />
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//      <div className="cardContainer">
+//      <div className="cardsSection" ref={boxRef}>
+//         {getRandomProducts().map((ele) => {
+//           return (
+//             <Card
+//               id={ele.id}
+//               image={ele.image}
+//               price={ele.price}
+//               title={ele.title}
+//             />
+//           );
+//         })}
+//       </div>
+//      </div>
+//     </div>
+//   );
+// };
+
+// export default SalesSection;
+
 import React, { useEffect, useRef, useState } from "react";
 import "./saleSection.css";
 import Card from "./Card";
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-// import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-// import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import axios from "axios";
-// import Products from './Data'
+
 
 const SalesSection = () => {
   const [products, setProducts] = useState([]);
@@ -28,16 +116,14 @@ const SalesSection = () => {
   const btnpressnext = () => {
     if (boxRef.current) {
       let width = boxRef.current.clientWidth / 5;
-      boxRef.current.scrollLeft = boxRef.current.scrollLeft + width;
-      boxRef.current.scrollBy({ left: width, behavior: "smooth" });
+      boxRef.current.scrollLeft += width;
     }
   };
 
   const btnpressprev = () => {
     if (boxRef.current) {
       let width = boxRef.current.clientWidth / 5;
-      boxRef.current.scrollLeft = boxRef.current.scrollLeft - width;
-      boxRef.current.scrollBy({ left: -width, behavior: "smooth" });
+      boxRef.current.scrollLeft -= width;
     }
   };
 
@@ -69,21 +155,23 @@ const SalesSection = () => {
         </div>
       </div>
      <div className="cardContainer">
-     <div className="cardsSection" ref={boxRef}>
-        {getRandomProducts().map((ele) => {
-          return (
-            <Card
-              id={ele.id}
-              image={ele.image}
-              price={ele.price}
-              title={ele.title}
-            />
-          );
-        })}
-      </div>
+       <div className="cardsSection" ref={boxRef} >
+          {getRandomProducts().map((ele) => {
+            return (
+              <Card
+                key={ele.id}
+                id={ele.id}
+                image={ele.image}
+                price={ele.price}
+                title={ele.title}
+              />
+            );
+          })}
+        </div>
      </div>
     </div>
   );
 };
 
 export default SalesSection;
+
